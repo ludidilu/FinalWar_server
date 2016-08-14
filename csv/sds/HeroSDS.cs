53dfca8ac9d084ff32f97c4f8f30d83e5b0cad09
Land <ludidilu@163.com>
2016-08-15 00:48:27 +0800

    a

--- csv/sds/HeroSDS.cs
+++ csv/sds/HeroSDS.cs
@@ -1,51 +1,34 @@
 ﻿public class HeroSDS : CsvBase,IHeroSDS
 {
     public int hp;
-    public int attackTimes;
-    public int damage;
-    public int power;
     public int cost;
-    public int heroType;
-    public int[] skills;
+    public int atk;
+    public int shoot;
+    public int def;
 
-    public HeroTypeSDS heroTypeSDS;
-
-    public override void Fix()
+    public int GetID()
     {
-        heroTypeSDS = StaticData.GetData<HeroTypeSDS>(heroType);
+        return ID;
     }
-
     public int GetHp()
     {
         return hp;
-    }
-    public int GetAttackTimes()
-    {
-        return attackTimes;
-    }
-    public int GetDamage()
-    {
-        return damage;
-    }
-    public int GetPower()
-    {
-        return power;
     }
     public int GetCost()
     {
         return cost;
     }
-    public int GetHeroType()
+    public int GetAtk()
     {
-        return heroType;
+        return atk;
     }
-    public IHeroTypeSDS GetHeroTypeSDS()
+    public int GetShoot()
     {
-        return heroTypeSDS;
+        return shoot;
     }
-    public int[] GetSkills()
+    public int GetDef()
     {
-        return skills;
+        return def;
     }
 }
 
