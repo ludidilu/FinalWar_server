ebd948d9b8d6307615b1f34481d9a918b63ef33a
ludi <ludidilu@163.com>
2017-08-25 17:48:34 +0800

    a

--- csv/sds/AuraSDS.cs
+++ csv/sds/AuraSDS.cs
@@ -1,13 +1,37 @@
 ﻿public partial class AuraSDS : CsvBase, IAuraSDS
 {
     public string eventName;
+    public int[] auraCondition;
+    public int[] auraConditionData;
     public int auraType;
     public int auraTarget;
     public int[] auraData;
 
+    private AuraCondition[] auraConditionFix;
+
+    public override void Fix()
+    {
+        auraConditionFix = new AuraCondition[auraCondition.Length];
+
+        for (int i = 0; i < auraCondition.Length; i++)
+        {
+            auraConditionFix[i] = (AuraCondition)auraCondition[i];
+        }
+    }
+
     public string GetEventName()
     {
         return eventName;
+    }
+
+    public AuraCondition[] GetAuraCondition()
+    {
+        return auraConditionFix;
+    }
+
+    public int[] GetAuraConditionData()
+    {
+        return auraConditionData;
     }
 
     public AuraType GetAuraType()
