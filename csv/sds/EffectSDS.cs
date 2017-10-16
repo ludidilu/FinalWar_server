eb800fbec12b88411255b68bab785ff68517675f
ludi <ludidilu@163.com>
2017-10-16 18:00:19 +0800

    a

--- csv/sds/EffectSDS.cs
+++ csv/sds/EffectSDS.cs
@@ -1,17 +1,11 @@
 ﻿public class EffectSDS : CsvBase, IEffectSDS
 {
     public int effect;
-    public int effectPriority;
     public int[] data;
 
     public Effect GetEffect()
     {
         return (Effect)effect;
-    }
-
-    public int GetEffectPriority()
-    {
-        return effectPriority;
     }
 
     public int[] GetData()
