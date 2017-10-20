94873eee182607f621fc6110ce15c1456952dd14
Land <ludidilu@163.com>
2017-10-21 01:58:46 +0800

    a

--- csv/sds/EffectSDS.cs
+++ csv/sds/EffectSDS.cs
@@ -1,11 +1,17 @@
-﻿public class EffectSDS : CsvBase, IEffectSDS
+﻿public partial class EffectSDS : CsvBase, IEffectSDS
 {
     public int effect;
+    public int priority;
     public int[] data;
 
     public Effect GetEffect()
     {
         return (Effect)effect;
+    }
+
+    public int GetPriority()
+    {
+        return priority;
     }
 
     public int[] GetData()
