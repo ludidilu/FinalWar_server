6f3b333d4130890cb9885b55c8dbfed630e21197
ludidilu <ludidilu@163.com>
2018-04-23 11:09:50 +0800

    a

--- csv/StaticData.cs
+++ csv/StaticData.cs
@@ -3,8 +3,9 @@ using System.IO;
 using System;
 using System.Reflection;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
-public class StaticData
+public static class StaticData
 {
     public static string path;
 
@@ -18,73 +19,53 @@ public class StaticData
     {
         Dictionary<int, T> tmpDic = dic[typeof(T)] as Dictionary<int, T>;
 
-        if (!tmpDic.ContainsKey(_id))
+        T data;
+
+        if (!tmpDic.TryGetValue(_id, out data))
         {
             Console.WriteLine(typeof(T).Name + "表中未找到ID为:" + _id + "的行!");
         }
 
-        return tmpDic[_id];
-    }
-
-    public static T GetDataOfKey<T>(string key, object keyValueParam) where T : CsvBase
-    {
-        Dictionary<int, T> dict = GetDic<T>();
-
-        Type type = typeof(T);
-
-        FieldInfo field = type.GetField(key);
-
-        foreach (T item in dict.Values)
-        {
-            object keyValue = field.GetValue(item);
-
-            if (keyValue.Equals(keyValueParam))
-            {
-                return item;
-            }
-        }
-
-        return default(T);
+        return data;
     }
 
     public static Dictionary<int, T> GetDic<T>() where T : CsvBase
     {
         Type type = typeof(T);
 
-        if (!dic.ContainsKey(type))
+        IDictionary data;
+
+        if (!dic.TryGetValue(type, out data))
         {
             Console.WriteLine("not find: " + type);
         }
 
-        return dic[type] as Dictionary<int, T>;
+        return data as Dictionary<int, T>;
     }
 
     public static List<T> GetList<T>() where T : CsvBase
     {
         Type type = typeof(T);
 
-        if (dicList.ContainsKey(type))
-        {
-            return dicList[type] as List<T>;
-        }
-        else
+        IList data;
+
+        if (!dicList.TryGetValue(type, out data))
         {
             Dictionary<int, T> dict = GetDic<T>();
 
-            List<T> list = new List<T>();
+            data = new List<T>();
 
             IEnumerator<T> enumerator = dict.Values.GetEnumerator();
 
             while (enumerator.MoveNext())
             {
-
-                list.Add(enumerator.Current);
+                data.Add(enumerator.Current);
             }
 
-            dicList.Add(type, list);
-
-            return list;
+            dicList.Add(type, data);
         }
+
+        return data as List<T>;
     }
 
     public static bool IsIDExists<T>(int _id) where T : CsvBase
@@ -126,7 +107,7 @@ public class StaticData
                     {
                         if (i == 2)
                         {
-                            string[] dataArr = lineStr.Split(',');
+                            string[] dataArr = SplitCsvLine(lineStr);
 
                             infoArr = new FieldInfo[dataArr.Length];
 
@@ -137,7 +118,7 @@ public class StaticData
                         }
                         else if (i > 2)
                         {
-                            string[] dataArr = lineStr.Split(',');
+                            string[] dataArr = SplitCsvLine(lineStr);
 
                             T csv = new T();
 
@@ -171,8 +152,6 @@ public class StaticData
 
     private static void SetData(FieldInfo _info, CsvBase _csv, string _data)
     {
-        string str = "setData:" + _info.Name + "   " + _info.FieldType.Name + "   " + _data + "   " + _data.Length + Environment.NewLine;
-
         try
         {
             switch (_info.FieldType.Name)
@@ -185,14 +164,14 @@ public class StaticData
                     }
                     else
                     {
-                        _info.SetValue(_csv, Int32.Parse(_data));
+                        _info.SetValue(_csv, int.Parse(_data));
                     }
 
                     break;
 
                 case "String":
 
-                    _info.SetValue(_csv, _data);
+                    _info.SetValue(_csv, FixStringChangeLine(_data));
 
                     break;
 
@@ -215,6 +194,32 @@ public class StaticData
 
                     break;
 
+                case "Double":
+
+                    if (string.IsNullOrEmpty(_data))
+                    {
+                        _info.SetValue(_csv, 0);
+                    }
+                    else
+                    {
+                        _info.SetValue(_csv, double.Parse(_data));
+                    }
+
+                    break;
+
+                case "Int16":
+
+                    if (string.IsNullOrEmpty(_data))
+                    {
+                        _info.SetValue(_csv, 0);
+                    }
+                    else
+                    {
+                        _info.SetValue(_csv, short.Parse(_data));
+                    }
+
+                    break;
+
                 case "Int32[]":
 
                     int[] intResult;
@@ -227,7 +232,7 @@ public class StaticData
 
                         for (int i = 0; i < strArr.Length; i++)
                         {
-                            intResult[i] = Int32.Parse(strArr[i]);
+                            intResult[i] = int.Parse(strArr[i]);
                         }
                     }
                     else
@@ -245,7 +250,15 @@ public class StaticData
 
                     if (!string.IsNullOrEmpty(_data))
                     {
-                        stringResult = _data.Split('$');
+                        string[] tmpStr = _data.Split('$');
+
+                        stringResult = new string[tmpStr.Length];
+
+                        for (int i = 0; i < tmpStr.Length; i++)
+                        {
+
+                            stringResult[i] = FixStringChangeLine(tmpStr[i]);
+                        }
                     }
                     else
                     {
@@ -280,7 +293,7 @@ public class StaticData
 
                     break;
 
-                default:
+                case "Single[]":
 
                     float[] floatResult;
 
@@ -303,11 +316,131 @@ public class StaticData
                     _info.SetValue(_csv, floatResult);
 
                     break;
+
+                case "Double[]":
+
+                    double[] doubleResult;
+
+                    if (!string.IsNullOrEmpty(_data))
+                    {
+                        string[] strArr = _data.Split('$');
+
+                        doubleResult = new double[strArr.Length];
+
+                        for (int i = 0; i < strArr.Length; i++)
+                        {
+                            doubleResult[i] = double.Parse(strArr[i]);
+                        }
+                    }
+                    else
+                    {
+                        doubleResult = new double[0];
+                    }
+
+                    _info.SetValue(_csv, doubleResult);
+
+                    break;
+
+                case "Int16[]":
+
+                    short[] shortResult;
+
+                    if (!string.IsNullOrEmpty(_data))
+                    {
+                        string[] strArr = _data.Split('$');
+
+                        shortResult = new short[strArr.Length];
+
+                        for (int i = 0; i < strArr.Length; i++)
+                        {
+                            shortResult[i] = short.Parse(strArr[i]);
+                        }
+                    }
+                    else
+                    {
+                        shortResult = new short[0];
+                    }
+
+                    _info.SetValue(_csv, shortResult);
+
+                    break;
+
+                default:
+
+                    throw new Exception("csv表中的类型不支持反射  setData:" + _info.Name + "   " + _info.FieldType.Name + "   " + _data);
             }
         }
         catch (Exception e)
         {
+            string str = "setData:" + _info.Name + "   " + _info.FieldType.Name + "   " + _data + "   " + _data.Length + Environment.NewLine;
+
             Console.WriteLine(str + "   " + e.ToString());
         }
+    }
+
+    private static string FixStringChangeLine(string _str)
+    {
+        return _str.Replace("\\n", "\n");
+    }
+
+    private static readonly Regex reg = new Regex("\".*?\"");
+
+    private static string[] SplitCsvLine(string _str)
+    {
+        List<KeyValuePair<string, string>> list = null;
+
+        int index = 0;
+
+        MatchEvaluator me = delegate (Match _match)
+        {
+            if (list == null)
+            {
+                list = new List<KeyValuePair<string, string>>();
+            }
+
+            string result = string.Format("$replace:{0}$", index);
+
+            string str = _match.Value.Substring(1, _match.Length - 2);
+
+            list.Add(new KeyValuePair<string, string>(result, str));
+
+            index++;
+
+            return result;
+        };
+
+        string strFix = reg.Replace(_str, me);
+
+        string[] final = strFix.Split(',');
+
+        if (list != null)
+        {
+            index = 0;
+
+            KeyValuePair<string, string> replaceData = list[index];
+
+            for (int i = 0; i < final.Length; i++)
+            {
+                string ss = final[i];
+
+                if (ss == replaceData.Key)
+                {
+                    final[i] = replaceData.Value;
+
+                    index++;
+
+                    if (index < list.Count)
+                    {
+                        replaceData = list[index];
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        return final;
     }
 }
